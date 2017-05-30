1fff3cf20471d03267f542270b1bc49439dea9a3
JoyceDan <joyceq.sk8@gmail.com>
2017-05-30 01:04:41 -0400

    add main title for display

--- src/Users_CenterManagers/CenterManager.java
+++ src/Users_CenterManagers/CenterManager.java
@@ -1,5 +1,32 @@
 package Users_CenterManagers;
 
+import java.util.Scanner;
+
 public class CenterManager {
+	public static void main(String[] args) {
+		//arguments: -Djava.security.manager . new SecurityManager()
+		checkValidID();
+		
+		
+	}
+
+	private static void checkValidID() {
+		// TODO Auto-generated method stub
+		Boolean flag = true;
+		while(flag) {
+			System.out.println("----------Welcome To Our System-----------");
+			do{
+				System.out.println("-----Please input a valid manager ID------");
+				Configuration.managerID = new Scanner(System.in).next();
+			}while(!checkManagerIDexists(Configuration.managerID));
+			flag = false;
+		}
+	}
+
+	private static boolean checkManagerIDexists(String managerID) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 
 }
---
+++ src/Users_CenterManagers/Configuration.java
@@ -0,0 +1,12 @@
+package Users_CenterManagers;
+
+import RemoteInterface.CenterServerInterface;
+
+public class Configuration {
+		public static String managerID = null; //ceter + 4 number
+		public static CenterServerInterface RemoteSever;
+		public static String aHost = "localhost";
+		public static final int ServerPort_MTL = 8000;
+		public static final int ServerPort_LVL = 8000;
+		public static final int ServerPort_DDO = 8000;
+}
