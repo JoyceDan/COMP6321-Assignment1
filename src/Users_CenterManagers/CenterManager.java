16d377f50b874092da06ab9cec31b8abb215d20a
JoyceDan <joyceq.sk8@gmail.com>
2017-06-02 00:34:57 -0400

    add client config

--- src/Users_CenterManagers/CenterManager.java
+++ src/Users_CenterManagers/CenterManager.java
@@ -1,5 +1,8 @@
 package Users_CenterManagers;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -10,14 +13,16 @@ public class CenterManager {
 	public static void main(String[] args) {
 		//arguments: -Djava.security.manager . new SecurityManager()
 		System.out.println("----------Welcome To Our System-----------");
-		checkValidID();
-		if(checkExistsServer(Configuration.managerID)){
-			showMenu(Configuration.managerID);
-			//create the log file for manager
-		}else{
-			System.err.println("The managerID does not exists in our system, please try again!\n");
-			checkValidID();
-		}
+		Boolean FlagID = true;
+		do {
+			checkManagerIDFormat();
+			if (checkExistsServer(Configuration.managerID)) {
+				showMenu(Configuration.managerID);
+				FlagID = false;
+			}else{
+				System.err.println("The managerID does not exists in our system, please try again!\n");			
+			}
+		} while (FlagID);
 		
 	}
 
@@ -28,45 +33,60 @@ public class CenterManager {
 		System.out.println("1. Create a teacher record");
 		System.out.println("2. Create a student record");
 		System.out.println("3. Get the number of your tatal records");
-		System.out.println("4. Edit your record");
-		
+		System.out.println("4. Edit your record");	
 	}
 
 	private static boolean checkExistsServer(String managerID) {
 		// TODO Auto-generated method stub
-		if(managerID.equals("MTL10000")){
-			return true;
-		}else {
-			return false;
+//		if(managerID.equals("MTL10000")){
+//			return true;
+//		}else {
+//			return false;
+//		}
+		DatagramSocket aSocket = null;
+		int serverPort = 0;
+		
+		if (Configuration.managerID.substring(0, 3).equalsIgnoreCase("MTL")) {
+			serverPort = Configuration.ServerPort_MTL;
+		}else if(Configuration.managerID.substring(0, 3).equalsIgnoreCase("LVL")){
+			serverPort = Configuration.ServerPort_LVL;
+		}else if (Configuration.managerID.substring(0, 3).equalsIgnoreCase("DDO")) {
+			serverPort = Configuration.ServerPort_DDO;
 		}
+		
+		try {
+			aSocket = new DatagramSocket();
+			byte[] msg = managerID.getBytes();
+			InetAddress aHost = InetAddress.getByName(Configuration.aHost);
+			DatagramPacket request = new DatagramPacket(msg, msg.length, aHost, serverPort);
+			aSocket.send(request);
+			
+			byte[] buffer = new byte[1000];
+			DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
+			aSocket.receive(reply);
+			System.out.println("Reply: "+ new String(reply.getData()));	
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
 	}
 
-	private static void checkValidID() {
-		// TODO Auto-generated method stub
+	private static void checkManagerIDFormat() {
 		Boolean flag = true;
-		while(flag) {
-			do{
-				System.out.println("-----Please input a valid manager ID------");
-				Scanner sc = new Scanner(System.in);
-				Configuration.managerID = sc.next();
-			}while(!checkManagerIDexists(Configuration.managerID));
-			flag = false;
-		}
+		do {
+			System.out.println("-----Please input a valid manager ID------");
+			Scanner sc = new Scanner(System.in);
+			Configuration.managerID = sc.next();
+			Pattern IDmanager = Pattern.compile("^(MTL|LVL|DDO)(\\d{5})$");
+			Matcher matcherString = IDmanager.matcher(Configuration.managerID);
+			if (matcherString.find()) {
+				System.out.println("----------Now, check if the managerID exists in our system-----------\n");
+				flag = false;
+			}else {
+				System.err.println("The ManagerID format should be MTL|LVL|DDO + 5 dight number. eg: MTL10000\n");	
+			}
+		} while (flag);
 	}
-
-	private static boolean checkManagerIDexists(String managerID) {
-		// TODO Auto-generated method stub
-		Pattern IDmanager = Pattern.compile("^(MTL|LVL|DDO)(\\d{5})$");
-		Matcher matcherString = IDmanager.matcher(managerID);
-		if (matcherString.find()) {
-			System.out.println("----------Now, check if the managerID exists in our system-----------\n");
-			return true;
-		}else {
-			System.err.println("The ManagerID format should be MTL|LVL|DDO + 5 dight number. eg: MTL10000\n");
-			checkValidID();
-			return false;
-		}
-	}
-
 
 }
--- src/Users_CenterManagers/Configuration.java
+++ src/Users_CenterManagers/Configuration.java
@@ -5,7 +5,7 @@ import RemoteInterface.CenterServerInterface;
 public class Configuration {
 		public static String managerID = null; //ceter + 4 number
 		public static CenterServerInterface RemoteSever;
-		public static String aHost = "127.0.0.1";
+		public static String aHost = "localhost";
 		public static final int ServerPort_MTL = 8000;
 		public static final int ServerPort_LVL = 8001;
 		public static final int ServerPort_DDO = 8002;
